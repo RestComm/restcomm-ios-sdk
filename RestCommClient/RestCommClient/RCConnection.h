9e100dfce939ac8332454ca33b4dd81066151269
Antonis Tsakiridis <atsakirid@gmail.com>
2014-10-12 23:53:45 +0300

    Started working on the documentation (appledoc), but the tool seems to have some problems, so I wait for input from the developer. Also, started working on making the library work on the simulator -managed to do the build, but got some runtime issues

--- RestCommClient/RestCommClient/RCConnection.h
+++ RestCommClient/RestCommClient/RCConnection.h
@@ -11,12 +11,15 @@
 #import "RCConnectionDelegate.h"
 #import "SipManager.h"
 
+/**
+ Connection State
+ */
 typedef enum
 {
-	RCConnectionStatePending = 0,
-	RCConnectionStateConnecting,
-	RCConnectionStateConnected,
-	RCConnectionStateDisconnected
+	RCConnectionStatePending = 0,  /** Connection is in state pending */
+	RCConnectionStateConnecting,  /** Connection is in state connecting */
+	RCConnectionStateConnected,  /** Connection is in state connected */
+	RCConnectionStateDisconnected  /** Connection is in state disconnected */
 } RCConnectionState;
 
 
@@ -28,21 +31,63 @@ extern NSString* const RCConnectionIncomingParameterCallSIDKey;
 
 @class SipManager;
 
+/**
+ *  Represents call either incoming or outgoing
+ */
 @interface RCConnection : NSObject<SipManagerConnectionDelegate>
+/**
+ *  State of the connection
+ */
 @property RCConnectionState state;
+/**
+ *  True if connection is incoming; false otherwise
+ */
 @property (nonatomic, getter=isIncoming) BOOL incoming;
+/**
+ *  Connection parameters
+ */
 @property (nonatomic, readonly) NSDictionary* parameters;
+/**
+ *  Delegate that will be receiving RCConnection events
+ */
 @property (nonatomic, assign) id<RCConnectionDelegate> delegate;
+/**
+ *  Is connection currently muted?
+ */
 @property (nonatomic, getter = isMuted) BOOL muted;
 
 
-// #new
+/**
+ *  Initialize a new RCConnection object
+ *
+ *  @param delegate Delegate of RCConnection
+ *
+ *  @return Newly initialized object
+ */
 - (id)initWithDelegate:(id<RCConnectionDelegate>)delegate;
+/**
+ *  Accept an incoming connection that is ringing. The connection state changes to 'RCConnectionStateConnected'
+ */
 - (void)accept;
+/**
+ *  Ignore connection
+ */
 - (void)ignore;
+/**
+ *  Reject an incoming connection
+ */
 - (void)reject;
+/**
+ *  Disconnect a connection that is in state 'RCConnectionStateConnecting' or 'RCConnectionStateConnected'
+ */
 - (void)disconnect;
+/**
+ *  Send DTMF tones over a connection that is in state 'RCConnectionStateConnected'
+ *
+ *  @param digits A string of digits that will be sent
+ */
 - (void)sendDigits:(NSString*)digits;
+
 // avoid reference cycle
 @property (weak) SipManager * sipManager;
 
--- RestCommClient/RestCommClient/RCDevice.h
+++ RestCommClient/RestCommClient/RCDevice.h
@@ -12,11 +12,13 @@
 #import "RCConnectionDelegate.h"
 #import "SipManager.h"
 
-typedef enum
-{
-	RCDeviceStateOffline = 0,
-	RCDeviceStateReady,
-	RCDeviceStateBusy
+/**
+ Device State
+ */
+typedef enum {
+    RCDeviceStateOffline = 0,  /** Device is offline */
+	RCDeviceStateReady,  /** Device is ready to make and receive connections */
+	RCDeviceStateBusy  /** Device is busy */
 } RCDeviceState;
 
 extern NSString* const RCDeviceCapabilityIncomingKey;
@@ -30,23 +32,83 @@ extern NSString* const RCDeviceCapabilityClientNameKey;
 
 @class RCConnection;
 
+/**
+ *  Represents an abstraction of a communication device able to make and receive calls, send and receive messages etc
+ */
 @interface RCDevice : NSObject <SipManagerDeviceDelegate,NSURLConnectionDelegate>
-
+/**
+ *  Device state
+ */
 @property (nonatomic, readonly) RCDeviceState state;
+/**
+ *  Device capabilities
+ */
 @property (nonatomic, readonly) NSDictionary* capabilities;
+/**
+ *  Delegate that will be receiving RCDevice events
+ */
 @property (nonatomic, assign) id<RCDeviceDelegate> delegate;
+/**
+ *  Sound for incoming connections enabled
+ */
 @property (nonatomic) BOOL incomingSoundEnabled;
+/**
+ *  Sound for outgoing connections enabled
+ */
 @property (nonatomic) BOOL outgoingSoundEnabled;
+/**
+ *  Sound for disconnect enabled
+ */
 @property (nonatomic) BOOL disconnectSoundEnabled;
-
+/**
+ *  Initialize a new RCDevice object
+ *
+ *  @param capabilityToken Capability Token
+ *  @param delegate        Delegate of RCDevice
+ *
+ *  @return Newly initialized object
+ */
 - (id)initWithCapabilityToken:(NSString*)capabilityToken delegate:(id<RCDeviceDelegate>)delegate;
+/**
+ *  Start listening for incoming connections
+ */
 - (void)listen;
+/**
+ *  Stop listening for incoming connections
+ */
 - (void)unlisten;
+/**
+ *  Update Capability Token
+ *
+ *  @param capabilityToken New Capability Token
+ */
 - (void)updateCapabilityToken:(NSString*)capabilityToken;
+/**
+ *  Create an outgoing connection to an endpoint
+ *
+ *  @param parameters Connections such as the endpoint we want to connect to
+ *  @param delegate   The delegate object that will receive events when the connection state changes
+ *
+ *  @return An RCConnection object representing the new connection
+ */
 - (RCConnection*)connect:(NSDictionary*)parameters delegate:(id<RCConnectionDelegate>)delegate;
+/**
+ *  Send an instant message
+ *
+ *  @param message  Message text
+ *  @param receiver Message receiver
+ */
 - (void)sendMessage:(NSString*)message to:(NSDictionary*)receiver;
+/**
+ *  Disconnect all connections
+ */
 - (void)disconnectAll;
 
+/**
+ *  Update RCDevice parameters
+ *
+ *  @param params Dictionary of key/value pairs of the parameters that will be updated
+ */
 - (void) updateParams:(NSDictionary*)params;
 
 @end
