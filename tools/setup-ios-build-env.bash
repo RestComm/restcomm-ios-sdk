73d5d4b7fa04b5536df4446bd599f16cd1d2ffea
Antonis Tsakiridis <atsakirid@gmail.com>
2014-10-17 01:41:56 +0300

    changes in tools: Came up with a configuration script to setup the environment for various (i.e. i386, x86_64, armv7, armv7s) SIP Sofia library builds

---
+++ tools/setup-ios-build-env.bash
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+SDK="iphoneos"
+ARCH="armv7"
+
+if [[ $# -ne 2 ]]
+then
+	echo "Usage: ios.script.bash <sdk> <architecture>"
+	exit 1
+fi 
+
+if [[ $1 == "simulator" ]]
+then
+	SDK="iphonesimulator"
+fi
+
+ARCH=$2
+I386_FLAGS=""
+if [[ $2 == "i386" ]]
+then
+	I386_FLAGS="-m32"
+fi
+
+export DEVROOT="$(xcrun --sdk $SDK --show-sdk-platform-path)/Developer"
+export SDKROOT="$(xcrun --sdk $SDK --show-sdk-path)"
+
+if [[ $1 == "simulator" ]]
+then
+	export CC=$DEVROOT/usr/bin/gcc
+	export CXX=$DEVROOT/usr/bin/g++
+else
+	export CC="/usr/bin/clang"
+	export CXX="/usr/bin/clang++"
+fi
+
+export LD="${DEVROOT}/usr/bin/ld"
+
+if [[ $1 == "simulator" ]]
+then
+	# there was no 'ar' in simulator DEVROOT, so I used the OSX one and worked
+	export AR="/usr/bin/ar"
+else
+	export AR="${DEVROOT}/usr/bin/ar"
+fi
+
+export AS="${DEVROOT}/usr/bin/as"
+export NM="${DEVROOT}/usr/bin/nm"
+
+if [[ $1 == "simulator" ]]
+then
+	# there was no 'ranlib' in simulator DEVROOT, so I used the OSX one and worked
+	export RANLIB="/usr/bin/ranlib"
+else
+	export RANLIB="${DEVROOT}/usr/bin/ranlib"
+fi
+
+if [[ $1 == "simulator" ]]
+then
+	export LDFLAGS=${I386_FLAGS}" -L${SDKROOT}/usr/lib/  -miphoneos-version-min=7.0"
+else
+	export LDFLAGS="-L${SDKROOT}/usr/lib/"
+fi
+
+#export ARCH="armv7"
+export ARCH
+export CFLAGS=${I386_FLAGS}" -arch ${ARCH} -pipe -no-cpp-precomp -isysroot ${SDKROOT} -I${SDKROOT}/usr/include/"
+#export CFLAGS="-arch ${ARCH} -pipe -no-cpp-precomp -isysroot ${SDKROOT} -I${SDKROOT}/usr/include/"
+
+export CPPFLAGS="${CFLAGS}"
+export CXXFLAGS="${CFLAGS}"
+
+echo "---------------"
+echo "Using CC: $CC"
+echo "Using SDK: $SDK"
+echo "Using ARCH: $ARCH"
+echo "Using DEVROOT: $DEVROOT"
+echo "Using SDKROOT: $SDKROOT"
+echo "Using CFLAGS: $CFLAGS"
+echo "Using LDFLAGS: $LDFLAGS"
+echo "Using AR: $AR"
+echo "Using AS: $AS"
+echo "Using NM: $NM"
+echo "Using RANLIB: $RANLIB"
+echo "---------------"
+
