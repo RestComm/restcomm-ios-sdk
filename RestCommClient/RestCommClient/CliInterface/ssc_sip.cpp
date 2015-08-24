17fe9ae8156f37217ed958d585cc60a3fcf7f59a
Antonis Tsakiridis <atsakirid@gmail.com>
2015-08-24 12:03:43 +0300

    Fixes #207: Outgoing call from Messenger to Olympus remains ringing even after being answered. Fixes #212: Add license text to some files lacking it. Fixes #213: Fix local video location

--- RestCommClient/RestCommClient/CliInterface/ssc_sip.cpp
+++ RestCommClient/RestCommClient/CliInterface/ssc_sip.cpp
@@ -21,6 +21,28 @@
  *
  */
 
+/*
+ * TeleStax, Open Source Cloud Communications
+ * Copyright 2011-2015, Telestax Inc and individual contributors
+ * by the @authors tag.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ * For questions related to commercial use licensing, please contact sales@telestax.com.
+ *
+ */
+
 /**@file ssc_sip.c Interface towards libsofia-sip-ua.
  * 
  * @author Kai Vehmanen <kai.vehmanen@nokia.com>
@@ -188,7 +210,7 @@ ssc_t *ssc_create(su_home_t *home, su_root_t *root, const ssc_conf_t *conf, cons
     if (conf->ssc_contact)
         contact = conf->ssc_contact;
     else
-        contact = "sip:*:5090;transport=udp";
+        contact = "sip:*:5090;transport=tcp";
     
     /* step: launch the SIP stack */
     ssc->ssc_nua = nua_create(root,
