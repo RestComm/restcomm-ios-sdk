f7a4928f490f114e2a5dd580b26b205f5e29d450
Antonis Tsakiridis <antonis.tsakiridis@telestax.com>
2016-11-16 17:04:03 +0200

    Working on #503: Automate Reference Documentation generation from travis CI

--- scripts/generate-doc.bash
+++ scripts/generate-doc.bash
@@ -1,5 +1,9 @@
 #!/bin/bash
 #
-# Generate apple doc reference documentation
+# Generate apple doc reference documentation and update gh-pages branch
 
+git checkout gh-pages
+git rebase master
 appledoc -h --no-create-docset --project-name "Restcomm iOS SDK" --project-company Telestax --company-id com.telestax --output "./doc" --index-desc "RestCommClient/doc/index.markdown" RestCommClient/Classes/RC* RestCommClient/Classes/RestCommClient.h
+git commit -m "Update gh-pages"
+git push origin gh-pages
