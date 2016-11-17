6828624725a01a1c6c57b7d0a37dd9f852436684
Antonis Tsakiridis <antonis.tsakiridis@telestax.com>
2016-11-17 09:45:53 +0200

    Adding force push for gh-pages

--- scripts/generate-doc.bash
+++ scripts/generate-doc.bash
@@ -2,6 +2,7 @@
 #
 # Generate apple doc reference documentation, update & commit gh-pages branch and push gh-pages branch
 
+echo "Checking out gh-pages"
 git checkout gh-pages
 echo "Rebasing gh-pages to master"
 git rebase master
@@ -13,7 +14,7 @@ echo "Adding changes to staging area and committing"
 git add .
 # Only commit if there are differences
 git diff --quiet --exit-code --cached || git commit -m "Update gh-pages"
-echo "Pushing gh-pages to origin"
-git push origin gh-pages
+echo "Force-pushing gh-pages to origin"
+git push -f origin gh-pages
 echo "Done updating docs, checking out master"
 git checkout master
