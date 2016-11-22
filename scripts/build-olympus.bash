e6361522ab0e144be02b16e1afd2770c57a67d2c
Antonis Tsakiridis <antonis.tsakiridis@telestax.com>
2016-11-22 12:33:11 +0200

    Working on #508: Deploy Olympus to TestFairy via Travis CI

--- scripts/build-olympus.bash
+++ scripts/build-olympus.bash
@@ -2,17 +2,20 @@
 #
 # Build Olympus after updating version string and deploy
 
-#PLIST_BUDDY="/usr/libexec/PlistBuddy"
+PLIST_BUDDY="/usr/libexec/PlistBuddy"
 INFOPLIST_FILE="Examples/restcomm-olympus/restcomm-olympus/restcomm-olympus-Info.plist"
 
-echo "-- Build"
+echo "-- Install CocoaPod dependencies"
+pod install --project-directory=Examples/restcomm-olympus
+
+echo "-- Build Olympus"
 set -o pipefail && xcodebuild build -workspace Examples/restcomm-olympus/restcomm-olympus.xcworkspace -scheme restcomm-olympus -destination 'platform=iOS Simulator,name=iPhone SE,OS=10.0'
 
-echo "-- /usr/libexec/PlistBuddy"
+echo "-- Updating .plist version strings:\n\tCFBundleShortVersionString $BASE_VERSION\n\tCFBundleVersion ${VERSION_SUFFIX}+${TRAVIS_BUILD_NUMBER}"
 # Set base version
-/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $BASE_VERSION" "$INFOPLIST_FILE"
+$PLIST_BUDDY -c "Set :CFBundleShortVersionString $BASE_VERSION" "$INFOPLIST_FILE"
 # Set suffix
-/usr/libexec/PlistBuddy -c "Set :CFBundleVersion ${VERSION_SUFFIX}+${TRAVIS_BUILD_NUMBER}" "$INFOPLIST_FILE"
+$PLIST_BUDDY -c "Set :CFBundleVersion ${VERSION_SUFFIX}+${TRAVIS_BUILD_NUMBER}" "$INFOPLIST_FILE"
 
-echo "-- PlistBuddy"
-PlistBuddy
+#echo "-- PlistBuddy"
+#PlistBuddy
