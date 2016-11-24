b5404724286839983d73afb21772ab12fffc986d
Antonis Tsakiridis <antonis.tsakiridis@telestax.com>
2016-11-24 16:28:16 +0200

    Working on #508: Deploy Olympus to TestFairy via Travis CI

---
+++ Examples/test-xcode8/test-xcode8/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test-xcode8
+//
+//  Created by Antonis Tsakiridis on 11/24/16.
+//  Copyright © 2016 Telestax Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Examples/test-xcode8/test-xcode8/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  test-xcode8
+//
+//  Created by Antonis Tsakiridis on 11/24/16.
+//  Copyright © 2016 Telestax Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
